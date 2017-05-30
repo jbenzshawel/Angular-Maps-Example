c2a25fedd4a63377e4eeffe72b11161f1954c040
Benzshawel <E064242@mcgladrey.rsm.net>
2017-05-30 12:50:07 -0500

    updating google maps API

--- app/core/maps.service.js
+++ app/core/maps.service.js
@@ -7,36 +7,24 @@ var mapsModule = angular.module('core.maps', []);
 /// Factory Methods 
 mapsModule.
   factory('Maps', ['$http',
-    function($http) {
-        ///////////////////////////////////////////////////////////////
-        /// private props and methods 
-
-        var _baseBingUrl = "http://dev.virtualearth.net/REST/v1/Locations?";
-
-        var _bingApiKey =  "";
-
-        var _googleApiKey = "";
-
-        var _baseGoogleUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
-
-        // @param coutryRegion A string specifying the ISO country code. example AU
-        // @param postalCode A string specifying the ISO country code. example 98178
-        // @param addressLine A string specifying the street line of an address. example 1 Microsoft Way
-        // @returns string Bing Maps REST Locations API endpoint with api key
-        var _getBingRoute = function (countryRegion, postalCode, addressLine) {
-            return _baseBingUrl + "countryRegion=" + countryRegion + "&postalCode=" + postalCode 
-                    + "&addressLine=" + addressLine + "&key=" + _bingApiKey;
-        }
-
-        var _getGoogleRoute = function (address) {
-            return _baseGoogleUrl + "location=" + address + "&key=" + _googleApiKey;
-        }
+    function($http) {        
+        var mapsAPI = new MapsAPI();
 
         //////////////////////////////////////////////////////////////
         /// public props and methods
 
         var service = {};
         
+        service.getApiResult = function (res) {
+            if (res != null && typeof (res) === "object" &&
+              res.hasOwnProperty("status") && res.status === 200 &&
+              res.hasOwnProperty("data") && res.data != null) {
+                  return res;
+              }
+            
+            return null;
+        }
+
         // @param coutryRegion A string specifying the ISO country code. example AU
         // @param postalCode A string specifying the ISO country code. example 98178
         // @param addressLine A string specifying the street line of an address. example 1 Microsoft Way
@@ -47,12 +35,12 @@ mapsModule.
             try {
                 $http({
                 method: 'GET',
-                url: _getBingRoute(countryRegion, postalCode, addressLine)
+                url: mapsAPI.getBingRoute(countryRegion, postalCode, addressLine)
                 }).then(resCallback, function errorCallback(response) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
                     console.warn("Error from Bing maps API");
-                    console.warn(res);   
+                    console.warn(response);   
                 }); 
             } catch (e) {
                 console.warn(e);
@@ -65,12 +53,12 @@ mapsModule.
             try {
                 $http({
                 method: 'GET',
-                url: _getGoogleRoute(address)
+                url: mapsAPI.getGoogleRoute(address)
                 }).then(resCallback, function errorCallback(response) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
                     console.warn("Error from Google Maps API.");
-                    console.warn(res);   
+                    console.warn(response);   
                 }); 
             } catch (e) {
                 console.warn(e);
--- app/http-demo/http-demo.component.js
+++ app/http-demo/http-demo.component.js
@@ -7,37 +7,51 @@ angular.
     templateUrl: 'http-demo/http-demo.template.html',
     controller: ['$scope', 'Maps',
       function HttpDemoController($scope, Maps) {
+
         var self = this;
 
-        $scope.address = null;
+        $scope.addressBing = null;
         
-        $scope.city = null;
+        $scope.cityBing = null;
         
-        $scope.state = null;
+        $scope.stateBing = null;
         
-        $scope.postalCode = null;
+        $scope.postalCodeBing = null;
         
-        $scope.validAddress = false; 
+        $scope.validAddressBing = false; 
 
+        $scope.formattedAddressGoogle = null;
         
+        $scope.validAddressGoogle = false; 
 
         $scope.findAddress = function () {
-          Maps.getAddressBing("US", $scope.searchPostal, $scope.searchAddress, function (res) {
-            window.apiRes = res;        
-            if (res != null && typeof (res) === "object" &&
-              res.hasOwnProperty("status") && res.status === 200 &&
-              res.hasOwnProperty("data") && res.data != null) {
-                var address = res.data.resourceSets[0].resources[0].address;
-                $scope.address = address.addressLine;
-                $scope.city = address.locality;
-                $scope.state = address.adminDistrict;
-                $scope.postalCode = address.postalCode;
+          Maps.getAddressBing("US", $scope.searchPostal, $scope.searchAddress, function (res) {        
+             var apiResult = Maps.getApiResult(res);
+
+             if (apiResult != null) {
+                var address = apiResult.data.resourceSets[0].resources[0].address;
+                $scope.addressBing = address.addressLine;
+                $scope.cityBing = address.locality;
+                $scope.stateBing = address.adminDistrict;
+                $scope.postalCodeBing = address.postalCode;
 
                 if (res.data.resourceSets[0].resources[0].confidence === "High") {
-                  $scope.validAddress = true;
+                  $scope.validAddressBing = true;
                 }
-              }
+              } // end if apiResult != null
             });
+
+            Maps.getAddressGoogle($scope.searchAddress + "+" + $scope.searchPostal, function (res) {
+              var apiResult = Maps.getApiResult(res);
+
+              if (apiResult != null) {
+                $scope.formattedAddressGoogle = apiResult.data.results[0].formatted_address;
+
+                if ($scope.formattedAddressGoogle != null) {
+                  $scope.validAddressGoogle = true;
+                }
+              } // end if apiResult != null
+            });            
         } // end function $scope.findAddress
       } // end controller function 
     ] // end controller
